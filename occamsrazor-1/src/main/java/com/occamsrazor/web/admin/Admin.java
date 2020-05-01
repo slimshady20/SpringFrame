9566db61b15502a83458a19ee8781afc3d83163f
slimshady20 <jayden6830@gmail.com>
2020-05-01 14:49:40 +0900

    0501 14:49

--- occamsrazor-1/src/main/java/com/occamsrazor/web/admin/Admin.java
+++ occamsrazor-1/src/main/java/com/occamsrazor/web/admin/Admin.java
@@ -1,8 +1,10 @@
 package com.occamsrazor.web.admin;
 
+import org.springframework.stereotype.Component;
+
 import lombok.Getter;
 import lombok.Setter;
-
+@Component
 @Getter @Setter
 public class Admin {
 private String name,position,email,phoneNumber,employNumber,passwd,profile,registerDate;
